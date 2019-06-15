05a3af3c472d76c3893fdcc03266124c508ccab9
mailer58 <50555861+mailer58@users.noreply.github.com>
2019-06-15 22:50:38 +0300

    Задание 7
    Декоративные элементы

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-function changeInput(input) {
-  var range=document.getElementById('range-input');
-  var input=document.getElementById('to-field');
-  input.value=range.value;
-}
-function changeSlider(range) {
-  var range=document.getElementById('range-input');
-  var input=document.getElementById('to-field');
-  range.value=input.value;
-}
+
+var open = document.querySelector(".btn-message");
+var popup = document.querySelector(".popup-window");
+var close = popup.querySelector(".message-close");
+var name = popup.querySelector("[name=name-field]");
+
+open.addEventListener("click", function (evt) {
+evt.preventDefault();
+popup.classList.add("popup-window-show");
+name.focus();
+  });
+
+  close.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   popup.classList.remove("popup-window-show");
+ });
